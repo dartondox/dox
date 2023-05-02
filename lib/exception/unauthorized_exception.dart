ba63ca0ff4a76ad5c51acc56fb179fabd43f150f
AJ <aj.zinkyaw@gmail.com>
2023-05-02 22:35:37 +0700

    bug fixed on exception

--- lib/exception/unauthorized_exception.dart
+++ lib/exception/unauthorized_exception.dart
@@ -2,19 +2,14 @@ import 'dart:io';
 
 import 'package:dox_core/dox_core.dart';
 
-class AuthorizationException extends BaseHttpException {
-  @override
-  final int code;
-
-  @override
-  final String errorCode;
-
-  @override
-  final String message;
-
-  AuthorizationException({
-    this.message = 'Failed to authorization',
-    this.errorCode = 'unauthorized',
-    this.code = HttpStatus.unauthorized,
-  });
+class UnAuthorizedException extends BaseHttpException {
+  UnAuthorizedException({
+    message = 'Failed to authorize',
+    errorCode = 'unauthorized',
+    code = HttpStatus.unauthorized,
+  }) {
+    super.code = code;
+    super.errorCode = errorCode;
+    super.message = message;
+  }
 }
--- lib/exception/validation_exception.dart
+++ lib/exception/validation_exception.dart
@@ -3,18 +3,13 @@ import 'dart:io';
 import 'package:dox_core/dox_core.dart';
 
 class ValidationException extends BaseHttpException {
-  @override
-  final int code;
-
-  @override
-  final String errorCode;
-
-  @override
-  final String message;
-
   ValidationException({
-    this.message = 'Validation failed',
-    this.errorCode = 'validation_failed',
-    this.code = HttpStatus.unprocessableEntity,
-  });
+    message = 'Validation failed',
+    errorCode = 'validation_failed',
+    code = HttpStatus.unprocessableEntity,
+  }) {
+    super.code = code;
+    super.errorCode = errorCode;
+    super.message = message;
+  }
 }
