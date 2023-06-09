32613e7f579ca21b3e8ae59468fb9341feacf9da
AJ <aj.zinkyaw@gmail.com>
2023-06-09 08:13:59 +0700

    added test case for resource route with missing method

---
+++ test/integration/requirements/controllers/blog.controller.dart
@@ -0,0 +1,33 @@
+import 'package:dox_core/dox_core.dart';
+
+class BlogController {
+  /// GET /admins
+  Future<String> index(DoxRequest req) async {
+    return 'GET /admins';
+  }
+
+  /// GET /admins/create
+  String create(DoxRequest req) {
+    return 'GET /admins/create';
+  }
+
+  /// GET /admins/{id}
+  Future<String> show(DoxRequest req, String id) async {
+    return 'GET /admins/{id}';
+  }
+
+  /// GET /admins/{id}/edit
+  Future<String> edit(DoxRequest req, String id) async {
+    return 'GET /admins/{id}/edit';
+  }
+
+  /// PUT|PATCH /admins/{id}
+  Future<String> update(DoxRequest req, String id) async {
+    return 'PUT|PATCH /admins/{id}';
+  }
+
+  /// DELETE /admins/{id}
+  Future<String> destroy(DoxRequest req, String id) async {
+    return 'DELETE /admins/{id}';
+  }
+}
