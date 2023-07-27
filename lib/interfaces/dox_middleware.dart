0d18c6a9b13cdffa6fde457fecce134f9cbe7d40
AJ <aj.zinkyaw@gmail.com>
2023-07-27 17:47:33 +0700

    ignore interfaces

--- lib/interfaces/dox_middleware.dart
+++ lib/interfaces/dox_middleware.dart
@@ -1,3 +1,4 @@
+/// coverage:ignore-file
 import 'package:dox_core/dox_core.dart';
 
 abstract class DoxMiddleware {
--- lib/interfaces/dox_service.dart
+++ lib/interfaces/dox_service.dart
@@ -1,3 +1,4 @@
+/// coverage:ignore-file
 abstract class DoxService {
   dynamic setup() {}
 }
