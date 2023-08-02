3ba8b9d50fd230e22a7de6a56db11dd1c9202f21
(AJ) Zin Kyaw <aj.zinkyaw@gmail.com>
2023-08-02 07:59:17 +0700

    bug fixed websocket on multiple isolates (#64)

---
+++ lib/websocket/event.dart
@@ -0,0 +1,10 @@
+class WebSocketEmitEvent {
+  final String? senderId;
+  final String roomId;
+  final dynamic message;
+  final String event;
+  final List<String> exclude;
+
+  const WebSocketEmitEvent(
+      this.senderId, this.roomId, this.message, this.event, this.exclude);
+}
--- lib/websocket/socket_emitter.dart
+++ lib/websocket/socket_emitter.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:dox_core/dox_core.dart';
 import 'package:dox_core/utils/json.dart';
 import 'package:dox_core/utils/logger.dart';
+import 'package:dox_core/websocket/event.dart';
 import 'package:dox_core/websocket/socket_storage.dart';
 import 'package:dox_core/websocket/web_socket_info.dart';
 
@@ -13,10 +14,14 @@ class SocketEmitter {
   /// sender socket id
   final String? sender;
 
+  /// where it is sending from
+  /// via callback or via isolate
+  final String via;
+
   /// storage to get active connection
   final SocketStorage _storage = SocketStorage();
 
-  SocketEmitter({this.sender, this.roomId});
+  SocketEmitter({this.sender, this.roomId, this.via = 'callback'});
 
   /// set room to sent message
   /// ```
@@ -52,6 +57,17 @@ class SocketEmitter {
       return;
     }
     List<String> members = _storage.getWebSocketIdsOfTheRoom(roomId!);
+
+    /// If sending from callback, it mean we need to notify other isolates.
+    /// So, sending to other isolates to get active connections of the rooms and
+    /// sent message to correct clients
+    if (via == 'callback' && Dox().sendPort != null) {
+      Dox().sendPort?.send(
+            WebSocketEmitEvent(sender, roomId!, message, event, exclude),
+          );
+      return;
+    }
+
     for (String socketId in members) {
       if (!exclude.contains(socketId)) {
         WebSocketInfo? info = _storage.getWebSocketInfo(socketId);
