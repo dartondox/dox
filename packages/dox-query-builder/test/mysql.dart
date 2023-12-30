469406e40c40392317f5929f11e077a26ab56185
necessarylion <aj.zinkyaw@gmail.com>
2023-12-31 00:51:15 +0630

    try with user dox

--- packages/dox-query-builder/test/mysql.dart
+++ packages/dox-query-builder/test/mysql.dart
@@ -5,10 +5,11 @@ import 'package:mysql1/mysql1.dart';
 Future<MySqlConnection> mysqlConnection() {
   String host = Platform.environment['DB_HOST'] ?? 'localhost';
   int port = int.parse(Platform.environment['PORT'] ?? '3306');
+  String user = Platform.environment['USER'] ?? 'root';
   ConnectionSettings settings = ConnectionSettings(
     host: host,
     port: port,
-    user: 'root',
+    user: user,
     password: 'password',
     db: 'dox-framework',
   );
